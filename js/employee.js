5762641f5aa8f35c09262b5927a007fc540853ef
eyo.omat@gmail.com <eyo.omat@gmail.com>
2017-01-12 15:44:23 +0100

    create properties for Employee class

--- js/employee.js
+++ js/employee.js
@@ -1,1 +1,44 @@
 'use strict'
+
+var Employee = function (employeeID, fname, sname, phone, employeeType, experiencLevel) {
+	this.employeeID = employeeID;
+	this.fname = fname;
+	this.sname = sname;
+	this.phone = phone;
+	this.employeeType = employeeType;
+	this.experiencLevel = experiencLevel;
+	this.saveEmployee = function () {
+		
+	};
+	this.getEmployee = function () {
+		return this;
+	};
+}
+
+module.exports = Employee;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
